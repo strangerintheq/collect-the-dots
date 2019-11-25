a86b6d6f8eb11c63cdd71f841153eae4c9a20d96
Konstantin Balashev <tripmannn@gmail.com>
2019-11-25 18:13:32 +0300

    Update BotDanger.js

--- dangers/BotDanger.js
+++ dangers/BotDanger.js
@@ -1,6 +1,16 @@
 class BotDanger extends Danger {
     constructor() {
         super('bot_');
+        this.direction = [0, 1];
+        this.delay = 1000;
+        this.t = Date.now();
+    }
+
+    tickDanger(cell, t){
+       if (t-this.t>this.delay){
+           delete cell.danger[this.id]:
+           cell.neighbors[0].addDanger(new BotDanger());
+       }
     }
 
     renderDanger(renderer, context,x,y) {
